d3789b0f02e3362db50ae3d3c4aea0f3f58cecc2
starsworld <snack2050@qq.com>
2020-01-09 22:45:29 +0800

    config spa

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const history = require('connect-history-api-fallback');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(history());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
