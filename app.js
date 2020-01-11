f889cbb33ace6eafdad069f7742958fb40729c22
starsworld <star19921991@163.com>
2020-01-11 19:40:41 +0800

    feature: add single page

--- app.js
+++ app.js
@@ -13,11 +13,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
